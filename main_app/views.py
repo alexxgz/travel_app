7bb9b212f8a782aa33b835b1cfe0040e60255740
platothegeek <platothegeek@gmail.com>
2021-01-14 11:50:40 -0800

    Building out sign up and login functionality - debugging

--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,18 @@ def home(request):
 
 def about(request):
   return HttpResponse('<h1>About</h1>')
+
+
+def signup(request):
+  error_message=''
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up...'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'home.html', context)

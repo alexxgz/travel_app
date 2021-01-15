f3f74224bb9dc7f76594cfa049545826434c4ee9
alexgz <alexinc19@gmail.com>
2021-01-15 15:17:32 -0800

    Added routes for user profile

--- main_app/views.py
+++ main_app/views.py
@@ -58,13 +58,25 @@ posts = [
 ]
 
 
-# def profile(request):
-#   # current_user = request.user
-#   if request.user.is_authenticated:
-#     context = { 'user': user }
-#     return render(request, 'user/profile.html',context)
-#   else: 
-#     return redirect('accounts/signup')
+def profile(request):
+    current_user = request.user
+    context = {'posts': posts}
+    if request.user.is_authenticated:
+     return render(request, 'user/profile.html', context)
+    else: 
+     return redirect('accounts/signup')
+
+class Post:
+  def __init__(self, title, city, body):
+    self.title = title
+    self.city = city
+    self.body = body
+
+posts = [
+    Post('Great Tacos', 'San Francisco', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
+    Post('Great Pizza', 'New York', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
+
+]
 
 
 def signup(request):

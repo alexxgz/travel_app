fff0d89079a0813b3d13eb599542f2deb0b40876
Heejin Ko <goheejin@gmail.com>
2021-01-16 21:53:06 -0800

    complete task/8

--- main_app/views.py
+++ main_app/views.py
@@ -51,8 +51,8 @@ def posts_show(request):
 
 def cities_index(request):
   context = {
-    'cities': cities, 
-    'posts': posts
+    'cities': cities,
+    'posts': posts,
     }
   return render(request, 'cities/index.html', context)
 
@@ -78,8 +78,6 @@ posts = [
     Post('Great Pizza', 'New York', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
 
 ]
-
-
 
 
 def signup(request):

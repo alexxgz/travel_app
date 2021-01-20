dfed5592b186ecd548753fd5dd22240de3858ad4
platothegeek <platothegeek@gmail.com>
2021-01-19 17:10:38 -0800

    Checking everythin is up-to-date

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,5 +4,6 @@ from .models import Profile, Post, City
 # Register your models here.
 
 UserAdmin.list_display = ('username', 'email', 'is_active', 'date_joined', 'is_staff')
-
 admin.site.register(Profile, UserAdmin)
+admin.site.register(Post)
+admin.site.register(City)
--- main_app/models.py
+++ main_app/models.py
@@ -4,7 +4,7 @@ from datetime import date
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class City:
+class City(models.Model):
     def __init__(self, name, state):
         self.name = name 
         self.state = state
@@ -17,7 +17,7 @@ class Profile(AbstractUser):
     city = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
 
-class Post:
+class Post(models.Model):
     def __init__(self, title, city, body):
         self.title = title
         self.city = city
@@ -26,5 +26,4 @@ class Post:
 posts = [
     Post('Great Tacos', 'San Francisco', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
     Post('Great Pizza', 'New York', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
-
 ]

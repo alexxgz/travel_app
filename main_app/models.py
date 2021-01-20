d0b72317148afb914e4ceb52995f2cf9643b71ef
alexgz <alexinc19@gmail.com>
2021-01-19 17:26:30 -0800

    deleted comments

--- main_app/models.py
+++ main_app/models.py
@@ -16,17 +16,6 @@ class Profile(AbstractUser):
     city = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
 
-# class Post:
-#     def __init__(self, title, city, body):
-#         self.title = title
-#         self.city = city
-#         self.body = body
-
-# posts = [
-#     Post('Great Tacos', 'San Francisco', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
-#     Post('Great Pizza', 'New York', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
-
-# ]
 
 class Post(models.Model):
     title = models.CharField(max_length=100)

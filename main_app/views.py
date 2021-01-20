f39828ec5daf724c61c1fec923906a77d2dd59c6
Heejin Ko <goheejin@gmail.com>
2021-01-20 10:03:14 -0800

    before merge

--- main_app/views.py
+++ main_app/views.py
@@ -81,21 +81,21 @@ def cities_index(request):
 
 
    
-# class City:
-#   def __init__(self, name, state):
-#     self.name = name 
-#     self.state = state
+class City:
+  def __init__(self, name, state):
+    self.name = name 
+    self.state = state
 
-# cities = [
-#     City('San Francisco', 'California'),
-#     City('New York City', 'New York')
-# ]
+cities = [
+    City('San Francisco', 'California'),
+    City('New York City', 'New York')
+]
 
-# class Post:
-#   def __init__(self, title, city, body):
-#     self.title = title
-#     self.city = city
-#     self.body = body
+class Post:
+  def __init__(self, title, city, body):
+    self.title = title
+    self.city = city
+    self.body = body
 
 posts = [
     Post('Great Tacos', 'San Francisco', 'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'),
